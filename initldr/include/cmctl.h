8afa72848523c8549eea2bdb57aae929dccf34fc
skylar <runixyuyue@gmail.com>
2024-10-18 21:05:15 +0800

    add immigration header

--- initldr/include/cmctl.h
+++ initldr/include/cmctl.h
@@ -1,16 +1,17 @@
-#ifndef _CMCTL_H
-#define _CMCTL_H
+#ifndef CMCTL_H
+#define CMCTL_H
 #include "type.h"
 #include "ldrtype.h"
-#include "bstartparm.h"
-#include "vgastr.h"
+#include "imgmgrhead.h"
 #include "vgastr_t.h"
-#include "realparm.h"
 #include "fs.h"
-#include "graph.h"
+#include "realparm.h"
+#include "chkcpmm_t.h"
+#include "io.h"
+#include "vgastr.h"
 #include "inithead.h"
 #include "ldrkrlentry.h"
-#include "io.h"
 #include "chkcpmm.h"
-#include "chkcpmm_t.h"
+#include "graph.h"
+#include "bstartparm.h"
 #endif
